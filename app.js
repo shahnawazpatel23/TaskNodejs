313b5d7642d96caa1147661902d73bfaa8ac82cf
shahnawaz patel <shahnawazpatel23@gmail.com>
2025-01-10 14:50:26 +0530

    url change

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ transporter.verify(function(error, success) {
 
 
 const corsOptions = {
-  origin: 'http://localhost:5173', // Allow requests from this origin (frontend URL)
+  origin: 'https://nodejstask-front.vercel.app', // Allow requests from this origin (frontend URL)
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow specific HTTP methods
   allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
   credentials: true, // Allow credentials (if needed)
@@ -56,7 +56,7 @@ app.use(cors(corsOptions)); // Apply the CORS middleware
 
 // Explicitly add Access-Control-Allow-Origin header
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173'); // Set your frontend URL
+  res.header('Access-Control-Allow-Origin', 'https://nodejstask-front.vercel.app'); // Set your frontend URL
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   if (req.method === 'OPTIONS') {
